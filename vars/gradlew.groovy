f9e9a792fd4010e1594a7e428e92f4497314f7c2
Alexey Abashev <alexey@abashev.ru>
2018-02-20 00:33:08 +0300

    Add custom command for gradlew

---
+++ vars/gradlew.groovy
@@ -0,0 +1,20 @@
+#!/usr/bin/env groovy
+
+/**
+ * Use gradle wrapper for builds.
+ *
+ * @param commands
+ */
+def call(commands, collectTests = false) {
+    try {
+        withCredentials([
+                usernamePassword(credentialsId: 'artifactory-teamcity', usernameVariable: 'MAVEN_USER', passwordVariable: 'MAVEN_PASSWORD')
+        ]) {
+            sh "./gradlew -PmavenUser=\$MAVEN_USER -PmavenPassword=\$MAVEN_PASSWORD $commands"
+        }
+    } finally {
+        if (collectTests) {
+            junit testResults: '**/build/test-results/test/TEST-*.xml', allowEmptyResults: true
+        }
+    }
+}

a87630b4d72d4292ca501f9ce3c073989194645d
Alexey Abashev <alexey@abashev.ru>
2018-02-28 23:20:25 +0300

    Add commands for easier checkout and notify

---
+++ vars/paktorCheckout.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/env groovy
+
+/**
+ * Checkout with all lfs actions and submodules
+ *
+ * @param scm
+ */
+def call(scm) {
+    stage('Checkout') {
+        checkout([
+                $class: 'GitSCM',
+                branches: scm.branches,
+                extensions: scm.extensions + [[$class: 'GitLFSPull'], [$class: 'SubmoduleOption', parentCredentials: true, recursiveSubmodules: true]],
+                userRemoteConfigs: scm.userRemoteConfigs
+        ])
+    }
+}

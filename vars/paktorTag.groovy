0bc9036421ac6d1287e1410bafa7a960131ca2bf
Pedro Saraiva Guedes <pedro@gopaktor.com>
2019-03-22 17:48:42 +0000

    add tag script

---
+++ vars/paktorTag.groovy
@@ -0,0 +1,18 @@
+#!/usr/bin/env groovy
+
+/**
+ * Create an push an annotated tag for a paktor build
+ *
+ * @param tag name to tag build with 
+ */
+def call(tag) {
+
+  sh 'git config --local credential.helper "!p() { echo username=\\$GIT_USERNAME; echo password=\\$GIT_PASSWORD; }; p"'
+
+  sh 'git tag -m "master build: $tag" -a "$tag"'
+  withCredentials([
+    usernamePassword(credentialsId: 'github', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')
+  ]) {
+    sh 'git push origin $tag'
+  }
+}

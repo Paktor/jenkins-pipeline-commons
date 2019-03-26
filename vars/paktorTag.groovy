af6f0fbf0ecac31ef8b4764ed6c105700b345ae4
Pedro Saraiva Guedes <pedro@gopaktor.com>
2019-03-26 13:59:20 +0000

    fix tag script

--- vars/paktorTag.groovy
+++ vars/paktorTag.groovy
@@ -9,10 +9,10 @@ def call(tag) {
 
   sh 'git config --local credential.helper "!p() { echo username=\\$GIT_USERNAME; echo password=\\$GIT_PASSWORD; }; p"'
 
-  sh 'git tag -m "master build: $tag" -a "$tag"'
+  sh "git tag -m 'master build: $tag' -a '$tag'"
   withCredentials([
     usernamePassword(credentialsId: 'github', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')
   ]) {
-    sh 'git push origin $tag'
+    sh "git push origin $tag"
   }
 }
